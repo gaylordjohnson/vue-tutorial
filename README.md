751e6fb57c756a6454de5bba7e13782c2c291836
Gaylord Johnson <gaylordjohnson@users.noreply.github.com>
2018-05-19 21:44:55 -0400

    Finished video 6 of tutorial. Added friend editing functionality.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@ This tutorial is based on a series of YouTube videos here: https://www.youtube.c
 To run it, simply open the index.html file (no need to run local http server)
 
 *********** NOTE ***********
-As of the current commit, I've left off the tutorial on video #6 (https://www.youtube.com/watch?v=Oyr5X5HwXhM) at timestamp 3'0"
-(Just went through how to delete entries - both on the server and locally - and about to learn how to edit entries).
+As of theAs of the current commit, I've left off the tutorial on video #6 (https://www.youtube.com/watch?v=Oyr5X5HwXhM)
+As of the current commit, I've left off the tutorial on video #6 (https://www.youtube.com/watch?v=Oyr5X5HwXhM)
+As of the current commit, I've left off the tutorial on video #6 (https://www.youtube.com/watch?v=Oyr5X5HwXhM)
 
 ALSO NOTE: 
 I'm using dummy REST API server, which doesn't persist data, so deletions and edits won't wctually save the changes. 
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = new Vue({
   el: '#app',
   data: {
+    editFriend: null,
     friends: [],
   },
   mounted() {
@@ -18,17 +19,41 @@ const app = new Vue({
         .then(() => {
           this.friends.splice(index, 1);
         })
-    }
+    },
+    updateFriend(person) {
+      fetch('https://jsonplaceholder.typicode.com/users/' + person.id, {
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+      })
+        .then(() => {
+          this.editFriend = null;
+        })
+    },
   },
   template: `
     <div>
-      <p v-for='person, index in friends'>{{person.name}} 
-      <button v-on:click='deleteFriend(person.id, index)'>X</button>
-      </p>
+      <ul>
+        <li v-for='person, index in friends'> 
+          <div v-if="editFriend === person.id"> <!-- EDITING -->
+            (id: {{person.id}}) <input v-on:keyup.13="updateFriend(person)" v-model="person.name" /> <!-- key 13 is 'enter' -->
+            <button v-on:click="updateFriend(person)">Save</button>
+          </div>
+          <div v-else> <!-- DISPLAYING -->
+            (id: {{person.id}}) {{person.name}} 
+            <button v-on:click='editFriend = person.id'>Edit</button>
+            <button v-on:click='deleteFriend(person.id, index)'>X</button>
+          </div>
+        </li>
+      </ul>
     </div>
   `,
 });
 
+// GJ: Below is from the first 3-4 videos in this tutorial, where we separated friend into its own component
+//
 // Vue.component('friend-component', {
 //   props: ['friend'],
 //   filters: {
